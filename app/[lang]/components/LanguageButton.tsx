7d628ee27b753ce56df69c8e8590ce30f6ba45c2
Klaus Wagner <Klaus.DantasWagner@kimeta.de>
2023-02-05 18:01:27 +0100

    language button

--- app/[lang]/components/LanguageButton.tsx
+++ app/[lang]/components/LanguageButton.tsx
@@ -1,15 +1,39 @@
 import Image from "next/image";
-import imgDe from "@/public/assets/flags/de.jpg";
-import imgEn from "@/public/assets/flags/en.jpg";
+import imgDe from "@/public/assets/flags/ger.png";
+import imgEn from "@/public/assets/flags/usa.png";
 
 const LanguageButton: React.FC<{ lang: string }> = ({ lang }) => {
   return (
-    <div className="absolute top-4 right-8">
-      {lang === "de" ? (
-        <Image src={imgDe} alt="de" width={40} height={40} />
-      ) : (
-        <Image src={imgEn} alt="en" width={40} height={40} />
-      )}
+    <div className="flex justify-end lg:w-[210mm] m-auto">
+      <div className="group inline-block justify-end relative z-10">
+        <button className="py-2 px-4 rounded inline-flex gap-1 items-center">
+          {lang === "de" ? (
+            <a href="/de">
+              <Image src={imgDe} alt="de" width={40} height={40} />
+            </a>
+          ) : (
+            <a href="/en">
+              <Image src={imgEn} alt="en" width={40} height={40} />
+            </a>
+          )}
+          <svg
+            className="fill-current h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </button>
+        {lang === "en" ? (
+          <a href="/de" className="absolute py-2 px-4 group-hover:block">
+            <Image src={imgDe} alt="de" width={40} height={40} />
+          </a>
+        ) : (
+          <a href="/en" className="absolute py-2 px-4 group-hover:block">
+            <Image src={imgEn} alt="en" width={40} height={40} />
+          </a>
+        )}
+      </div>
     </div>
   );
 };
