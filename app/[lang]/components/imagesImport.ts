c24b6f55924286c59a8d38c552593a62c05161a3
Klaus Wagner <Klaus.DantasWagner@kimeta.de>
2023-02-05 18:02:32 +0100

    skills component

---
+++ app/[lang]/components/imagesImport.ts
@@ -0,0 +1,33 @@
+import typescript from "@/public/skills/typescript.jpg";
+import react from "@/public/skills/react.jpg";
+import next from "@/public/skills/next.jpg";
+import docker from "@/public/skills/docker.jpg";
+import jest from "@/public/skills/jest.jpg";
+import puppeteer from "@/public/skills/puppeteer.jpg";
+import node from "@/public/skills/node.jpg";
+import html from "@/public/skills/html.jpg";
+import php from "@/public/skills/php.jpg";
+import css from "@/public/skills/css.jpg";
+import tailwind from "@/public/skills/tailwind.jpg";
+import mongodb from "@/public/skills/mongodb.jpg";
+import graphql from "@/public/skills/graphql.jpg";
+import gitlab from "@/public/skills/gitlab.jpg";
+import vscode from "@/public/skills/vscode.jpg";
+
+export {
+  typescript,
+  react,
+  next,
+  docker,
+  jest,
+  puppeteer,
+  node,
+  html,
+  php,
+  css,
+  tailwind,
+  mongodb,
+  graphql,
+  gitlab,
+  vscode,
+};
