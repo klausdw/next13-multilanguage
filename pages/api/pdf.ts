04a124d59ef51f729cd19e1ffb0e8ad8704f1d51
Klaus Wagner <Klaus.DantasWagner@kimeta.de>
2023-02-11 12:00:34 +0100

    headers

--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -2,30 +2,41 @@ import { NextApiHandler } from "next";
 import puppeteer from "puppeteer";
 
 const Handler: NextApiHandler = async (req, res) => {
-    const lang = req.url?.split('/')[1]
+    try {
+        const lang = req.url?.split('/')[1]
 
-    const browser = await puppeteer.launch({
-        args: [
-            '--ignore-certificate-errors',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--window-size=1920,1080',
-            "--disable-accelerated-2d-canvas",
-            "--disable-gpu"],
-        ignoreHTTPSErrors: true,
-    })
-    const page = await browser.newPage()
-    const currentUrl = `${req.headers.host}`
-    const protocol = req.headers["x-forwarded-proto"] || "http"
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        );
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Content-Type', 'application/pdf');
 
-    await page.goto(`https://next13-multilanguage.vercel.app/${lang}`)
+        const browser = await puppeteer.launch({
+            args: [
+                '--ignore-certificate-errors',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--window-size=1920,1080',
+                "--disable-accelerated-2d-canvas",
+                "--disable-gpu"],
+            ignoreHTTPSErrors: true,
+        })
+        const page = await browser.newPage()
 
-    const buffer = await page.pdf({
-        format: 'a4',
-    })
-    res.end(buffer)
+        await page.goto(`https://next13-multilanguage.vercel.app/${lang}`)
 
-    await browser.close()
+        const buffer = await page.pdf({
+            format: 'a4',
+        })
+        res.end(buffer)
+
+        await browser.close()
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export default Handler
